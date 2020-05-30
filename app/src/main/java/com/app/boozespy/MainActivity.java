f191aaddaad03990d9737dc8cddd134332116172
Sean Moir <seanmoir99@gmail.com>
2020-05-31 04:49:12 +1200

    getting basic UI functionality

--- app/src/main/java/com/app/boozespy/MainActivity.java
+++ app/src/main/java/com/app/boozespy/MainActivity.java
@@ -1,13 +1,21 @@
 package com.app.boozespy;
 
+import android.view.View;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String address = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+     public void onEnter(View view) {
+         EditText editBox = findViewById(R.id.address);
+         address = editBox.getText().toString();
+         System.out.println(address);
+     }
 }
