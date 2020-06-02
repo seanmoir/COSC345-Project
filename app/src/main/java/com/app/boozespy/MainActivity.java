c3c79ef467899d4d2cc02fae225e3ce962e04ba2
Sean Moir <seanmoir99@gmail.com>
2020-06-03 00:56:54 +1200

    WIP integrating jsoup into app

--- app/src/main/java/com/app/boozespy/MainActivity.java
+++ app/src/main/java/com/app/boozespy/MainActivity.java
@@ -1,7 +1,11 @@
 package com.app.boozespy;
 
 import android.location.Location;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +13,16 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FusedLocationProviderClient locationClient;
@@ -26,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 .addOnFailureListener(this, new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        System.out.println(e);
+                        e.printStackTrace();
                     }
                 })
 
@@ -37,9 +51,53 @@ public class MainActivity extends AppCompatActivity {
                         if (location != null) {
                             gpsLocation.setText(location.toString());
                         } else {
+                            //debugging so we know if location is returned NULL
                             gpsLocation.setText("NULL");
                         }
                     }
                 });
+
+        final EditText search = findViewById(R.id.searchItem);
+        search.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER) &&
+                    (search.getText().length() > 0)) {
+                    // Perform action on key press
+                    try {
+                        Toast.makeText(MainActivity.this,
+                                LiqourLandProducts(search.getText().toString()).toString(), Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+
+    //Ubaada's web scrapping work integrated into the app by Sean
+    public static List<Product> LiqourLandProducts(String searchTerm) throws IOException {
+        List<Product> productList = new ArrayList<>();
+        String url = "https://www.shop.liquorland.co.nz/Search.aspx?k=" + searchTerm;
+        Map<String, String> cookies = new HashMap<>();
+        cookies.put("VisitorIsAdult", "True");
+        cookies.put("sessiondob", "01/01/1900");
+        cookies.put("selectedStore", "933");
+        Document doc = Jsoup.connect(url).cookies(cookies).get();
+
+        Elements scrappedList = doc.getElementsByClass("itemContainer");
+        for (Element product : scrappedList) {
+            Product newProd = new Product();
+            newProd.setName(product.selectFirst(".w2mItemName").text());
+            newProd.setPrice(Double.parseDouble(product.selectFirst("span.value , span.SpecialPriceFormat2").text().replace("$","")));
+            newProd.setImgUrl("http://www.shop.liquorland.co.nz/" + product.selectFirst("a img").attr("src"));
+            newProd.setUrl("http://www.shop.liquorland.co.nz/" + product.selectFirst("a").attr("href"));
+
+            productList.add(newProd);
+        }
+        return productList;
     }
 }
---
+++ app/src/main/java/com/app/boozespy/Product.java
@@ -0,0 +1,54 @@
+package com.app.boozespy;
+
+public class Product {
+    private String name;
+    private String imgUrl;
+    private String url;
+    private double price;
+
+    public Product() {
+        setName("");
+        setPrice(0.00);
+        setImgUrl("");
+        setUrl("");
+    }
+
+    public Product(String name, double price, String imgUrl, String url) {
+        setName(name);
+        setPrice(price);
+        setUrl(url);
+        setImgUrl(imgUrl);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
