5fbe8de5cf8d55badc51c9ec3624e25b8815444d
Sean Moir <seanmoir99@gmail.com>
2020-06-03 00:13:20 +1200

    location data working

--- app/src/main/java/com/app/boozespy/MainActivity.java
+++ app/src/main/java/com/app/boozespy/MainActivity.java
@@ -1,21 +1,45 @@
 package com.app.boozespy;
 
-import android.view.View;
-import android.widget.EditText;
+import android.location.Location;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
-    private String address = "";
+    private FusedLocationProviderClient locationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        final TextView gpsLocation = findViewById(R.id.gpsPrintOut);
+
+        locationClient.getLastLocation()
+                //print why failed to get location to stdout for debugging purposes
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println(e);
+                    }
+                })
+
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            gpsLocation.setText(location.toString());
+                        } else {
+                            gpsLocation.setText("NULL");
+                        }
+                    }
+                });
     }
-     public void onEnterAddress(View view) {
-         EditText editBox = findViewById(R.id.address);
-         address = editBox.getText().toString();
-         System.out.println(address);
-     }
 }
