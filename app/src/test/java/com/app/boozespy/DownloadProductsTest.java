2ef7acc69170ec05f7bea2beb17e0ae90d446dbb
Sean Moir <seanmoir99@gmail.com>
2020-06-05 03:05:27 +1200

    added documentation for tests

--- app/src/test/java/com/app/boozespy/DownloadProductsTest.java
+++ app/src/test/java/com/app/boozespy/DownloadProductsTest.java
@@ -7,19 +7,40 @@ import java.io.IOException;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
+/**
+ * Tests downloading of web data with a broad search
+ *
+ * @author Sean Moir
+ */
 public class DownloadProductsTest {
+
+    /**
+     * Checks New World data that it returns results
+     *
+     * @throws IOException connection issues
+     */
     @Test
     public void newWorldData_ReturnsNonEmpty() throws IOException {
         assertNotNull(DownloadProducts.NewWorldProducts("h"));
         assertNotEquals(DownloadProducts.NewWorldProducts("h").size(), 0);
     }
 
+    /**
+     * Checks Liqourland data that it returns results
+     *
+     * @throws IOException connection issues
+     */
     @Test
     public void liqourlandData_ReturnsNonEmpty() throws IOException {
         assertNotNull(DownloadProducts.LiqourLandProducts("h"));
         assertNotEquals(DownloadProducts.LiqourLandProducts("h").size(), 0);
     }
 
+    /**
+     * Checks Pak 'n' Save data that it returns results
+     *
+     * @throws IOException connection issues
+     */
     @Test
     public void paknSaveData_ReturnsNonEmpty() throws IOException {
         assertNotNull(DownloadProducts.PaknSaveProducts("h"));
